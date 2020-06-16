0272faa7fbf37bae591b67cd3f6733b3d2d56da9
Kasey <kcowley1988@gmail.com>
2020-06-15 23:28:21 -0600

    Set user agent

--- index.js
+++ index.js
@@ -45,6 +45,9 @@ const sendMail = (screenshot) => {
   });
   const page = await browser.newPage();
 
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36"
+  );
   await page.goto("https://www.t-mobile.com/cell-phone/oneplus-7-pro");
 
   const isInStock = await page.evaluate(() => {
